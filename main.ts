7f48fb1bd722ffe3b298b42de18dfad7219fafb9
DAIZAL <135332839+DaALRa8@users.noreply.github.com>
2023-06-02 13:58:09 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    jugadorArriba.change(LedSpriteProperty.Y, -1)
+    JugadorAbajo.change(LedSpriteProperty.Y, -1)
+})
+let JugadorAbajo: game.LedSprite = null
+let jugadorArriba: game.LedSprite = null
+jugadorArriba = game.createSprite(1, 3)
+JugadorAbajo = game.createSprite(1, 4)
+let obstáculo = game.createSprite(4, 4)
 basic.forever(function () {
 	
 })
